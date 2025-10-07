5bf33b6b671c538f05566a30c4fb6ac5130dfebc
tzebohn <tzebohnling@gmail.com>
2025-10-07 13:29:35 -0400

    Set up CartContext and useCart hook with basic provider

---
+++ src/contexts/useCart.js
@@ -0,0 +1,7 @@
+import { useContext } from "react";
+import { CartContext } from "./CartContext"
+
+// Custom hook
+export function useCart() {
+    return useContext(CartContext)
+}
--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,12 @@ import './index.css'
 import App from './App.jsx'
 import { RouterProvider } from 'react-router-dom'
 import router from './router.jsx'
+import { CartProvider } from './contexts/CartContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <CartProvider>
+      <RouterProvider router={router} />
+    </CartProvider>
   </StrictMode>,
 )
