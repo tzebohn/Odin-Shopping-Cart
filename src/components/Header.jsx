4dd34c85e26ff539f947e6be52a1f3eef51b851f
tzebohn <tzebohnling@gmail.com>
2025-10-07 22:34:48 -0400

    Show cart item count and make nav responsive
    Displayed number of cart items as a badge, updating dynamically.
    Added responsive styling to navigation links for better mobile layout.
    Changed company name to ShopEasy.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,46 @@
 import { FaShopify } from "react-icons/fa";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
+import { useCart } from "../contexts/useCart";
+import { IoCartOutline } from "react-icons/io5";
+
 export function Header () {
+
+    // Locate what url path the user is on currently
+    const location = useLocation()
+    const showSearch = location.pathname !== "/" && location.pathname !== "/cart" // Only show search bar in products tab
+
+    // CUSTOM HOOKS
+    const { cart } = useCart() 
+
+    // Calculate total items in cart
+    const totalQuantity = cart.reduce((sum, item) => sum + item.quantity, 0)
+
     return (
         <div className="bg-[#1f0d33] text-[#e8e8e8] flex items-center justify-between px-8 py-8">
             {/* Left: Logo */}
-            <div className="flex items-center gap-2">
+            <div className="flex items-center">
                 <FaShopify className="text-6xl"/>
-                <h1>EasyCart</h1>
+                <h1 className="text-2xl font-semibold">hopEasy</h1>
             </div>
 
-            {/* Middle: Search bar */}
-            <form>
-                <input type="text" placeholder="Search EasyCart"/>
+            {/* Middle: Conditional search bar */}
+            {showSearch && 
+            <form className="hidden sm:block">
+                <input type="text" placeholder="Search ShopEasy"/>
             </form>
+            }
 
             {/* Right: Navigation */}
-            <nav className="flex gap-6">
+            <nav className="hidden sm:flex items-center sm:gap-6">
                 <NavLink to={"/"}>Home</NavLink>
                 <NavLink to={"/products"}>Products</NavLink>
-                <NavLink to={"/cart"}>Cart</NavLink>
+                <div className="flex items-center gap-4">
+                    <NavLink to={"/cart"} className="relative">
+                        <IoCartOutline className="cursor-pointer text-[30px]"/>
+                        <span className="absolute -top-2 -right-3 bg-yellow-500 text-black text-xs font-bold rounded-full px-[6px] py-[2px]">{totalQuantity}</span>
+                    </NavLink>
+                    <NavLink to={"/cart"}>Cart</NavLink>
+                </div>
             </nav>
         </div>
     )
