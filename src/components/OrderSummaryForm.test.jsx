7ada3711e8117bdcbc3ff7f7295b1935e70d895e
tzebohn <tzebohnling@gmail.com>
2025-10-15 17:56:26 -0400

    Add basic test for OrderSummaryForm
    - Ensures that OrderSummaryForm renders correctly based on provided cart items.
    - 'Checkout' navigation behavior will be tested in the future when implemented.

---
+++ src/components/OrderSummaryForm.test.jsx
@@ -0,0 +1,52 @@
+import { describe, expect, it, vi } from "vitest";
+import { CartProvider } from "../contexts/CartProvider";
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+
+const mockNavigate = vi.fn()
+
+vi.mock("react-router-dom", async () => {
+    const actual = await vi.importActual("react-router-dom")
+    return {
+        ...actual,
+        useNavigate: () => mockNavigate
+    }
+})
+
+import { Cart } from "../pages/Cart";
+
+// Create a mock cart for testing 
+const mockCart = [
+    {
+        "id": 2,
+        "title": "Mens Casual Premium Slim Fit T-Shirts",
+        "price": 22.3,
+        "description": "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
+        "category": "men's clothing",
+        "image": "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_t.png",
+        "rating": {
+            "rate": 4.1,
+            "count": 259
+        },
+        "quantity": 1
+    },
+]
+
+describe("OrderSummary Form", () => {
+    it("renders OrderSummaryForm", () => {
+        render (
+            <BrowserRouter>
+                <CartProvider initialCart={mockCart}>
+                    <Cart/>
+                </CartProvider>
+            </BrowserRouter>
+        )
+
+        expect(screen.getByText(/items 1/i)).toBeInTheDocument()
+        expect(screen.getByLabelText(/shipping/i)).toBeInTheDocument()
+        expect(screen.getByLabelText(/promo code/i)).toBeInTheDocument()
+        expect(screen.getByRole("button", { name: /apply/i })).toBeInTheDocument()
+    })
+
+    // TODO: MAKE SURE CHECKOUT BUTTON NAVIGATES TO CORRECT PATH
+})
