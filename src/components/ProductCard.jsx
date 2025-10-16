8986e049be62a62db06fe4904c6af73a7b23d0a8
tzebohn <tzebohnling@gmail.com>
2025-10-16 02:39:17 -0400

    Fix padding and price display on product

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -35,11 +35,11 @@ export function ProductCard ({ product }) {
 
     return (
         <article className="bg-white shadow-sm rounded-xl overflow-hidden transition-shadow hover:shadow-lg">
-            <img src={`${product.image}`} alt={`${product.image}`} className="object-contain h-48 w-full" loading="lazy"/>
+            <img src={`${product.image}`} alt={`${product.image}`} className="object-contain h-48 w-full py-2" loading="lazy"/>
 
             <div className="flex flex-col p-4 gap-2">
                 <h2 className="truncate font-semibold md:text-lg lg:text-xl">{product.title}</h2>
-                <p className="font-medium text-gray-700">{`$${product.price}`}</p>
+                <p className="font-medium text-gray-700">{`$${product.price.toFixed(2)}`}</p>
                 {!inCart ? (
                     <button onClick={handleAdd} className="bg-yellow-300 hover:bg-yellow-400 rounded-full cursor-pointer p-2 px-4 md:self-start md:w-auto">Add to cart</button>
                 ) : (
