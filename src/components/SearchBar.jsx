2c2b5d2ee46816e3558428bd85dcb511b70d19d4
tzebohn <tzebohnling@gmail.com>
2025-10-17 00:18:24 -0400

    Connect SearchBar input to searchTerm

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,21 @@
+import { useMenu } from "../contexts/useMenu"
+import { useProducts } from "../contexts/useProducts"
+
 export function SearchBar ({ className = ""}) {
+
+    const { searchTerm, setSearchTerm } = useProducts()
+    const { setMenuOpen } = useMenu()
+
+    function handleSubmit (e) {
+        e.preventDefault()
+        setMenuOpen(false)
+    }
     return (
-        <form className={`${className}`}>
+        <form className={`${className}`} onSubmit={handleSubmit}>
             <input 
                 type="text" 
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
                 placeholder="Search ShopEasy"
                 className="outline-none p-2 px-4 bg-[#C0C0C0] rounded-full text-black placeholder-black text-semibold lg:w-md"
             />
