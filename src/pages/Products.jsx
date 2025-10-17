554a61cabaf110990a720986fdbd0298732b0e76
tzebohn <tzebohnling@gmail.com>
2025-10-17 00:19:15 -0400

    Filter product list based on searchTerm from context
    - Enables dynamic, real-time product search functionality.

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -2,13 +2,27 @@ import { useLoaderData } from "react-router-dom"
 import { ProductCard } from "../components/ProductCard";
 import { useMenu } from "../contexts/useMenu";
 import { MenuOverlay } from "../components/MenuOverlay";
+import { useProducts } from "../contexts/useProducts";
 
 export function Products () {
 
     const products = useLoaderData(); // Array of product objects
 
     const { menuOpen } = useMenu()
+    const { searchTerm } = useProducts()
 
+    // Filter products based on searchTerm
+    const filteredProducts = products.filter(p => {
+        if (!searchTerm) return true 
+        const term = searchTerm.toLowerCase()
+
+        return (
+            p.category.toLowerCase().includes(term) || 
+            p.description.toLowerCase().includes(term)
+        )
+    })
+
+    //console.log(filteredProducts)
     //console.log(products)
     return (
         <main className="relative p-8">
@@ -17,7 +31,7 @@ export function Products () {
             {menuOpen && <MenuOverlay />}
             
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
-                {products.map(product => (
+                {filteredProducts.map(product => (
                     <ProductCard 
                         key={product.id}
                         product={product}
